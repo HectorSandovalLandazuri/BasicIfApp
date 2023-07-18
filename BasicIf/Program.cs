8a0c7b44edc561970b0c18ffc505e2fa58b45af2
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-07-18 09:46:06 -0600

    Agregar archivos de proyecto.

---
+++ BasicIf/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+bool isComplete  = true;
+
+if (isComplete)
+{
+    Console.WriteLine("It's complete.");
+} else
+{
+    Console.WriteLine("It's not complete.");
+}
+
+Console.Write("What is your name: ");
+string firstName=Console.ReadLine();
+
+if (firstName.ToLower() =="héctor")
+{
+    Console.WriteLine("It's me.");
+} else
+{
+    Console.WriteLine($"Hello {firstName}.");
+}
+
+Console.WriteLine("End of Program.");
